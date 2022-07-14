649d2bdeaa3f0f4d3a020e104120acd65ce86daa
SILAS DE SALES <silasmsales@hotmail.com>
2022-07-14 15:18:15 -0300

    Add some style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <!-- Theme style -->
+      <link href="{{ asset('/assets/dist/css/adminlte.css') }}" rel="stylesheet">
+      <link href="{{ asset('/assets/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+      
     <title>@yield('title') - Curso</title>
 </head>
 <body>
     <div class="app">
         @yield('content')
     </div>
+
+<!-- jQuery -->
+<script src="/assets/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="/assets/dist/js/adminlte.min.js"></script>
+
 </body>
 </html>
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -9,19 +9,55 @@
     (<a href="{{ route('users.create') }}">+</a>)
 </h1>
 
-<form action="{{route('users.index')}}" method="get">
-    <input type="text" name="search" placeholder="Pesquisar">
-    <button type="submit">Pesquisar</button>
-</form>
+<div class="row">
+    <div class="col-12">
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title">Usuários</h3>
+          <div class="card-tools">
 
-<ul>
-    @foreach ($users as $user)
-    <li>
-        {{ $user->name }} -  
-        {{ $user->email }} - 
-        <a href="{{ route('users.show', $user->id) }}">Ver detalhes</a> -   
-        <a href="{{ route('users.edit', $user->id) }}">Editar</a> -   
-    </li>
-    @endforeach
-</ul>
+            <form action="{{route('users.index')}}" method="get">
+                <div class="input-group input-group-sm" style="width: 150px;">
+                <input type="text" name="search" class="form-control float-right" placeholder="Pesquisar">
+                <div class="input-group-append">
+                    <button type="submit" class="btn btn-default">
+                    <i class="fas fa-search"></i>
+                    </button>
+                </div>
+                </div>
+            </form>
+
+
+          </div>
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body table-responsive p-0">
+          <table class="table table-hover text-nowrap">
+            <thead>
+              <tr>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+             @foreach ($users as $user)
+                <tr>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>
+                        <a href="{{ route('users.show', $user->id) }}">Ver detalhes</a> -   
+                        <a href="{{ route('users.edit', $user->id) }}">Editar</a>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+          </table>
+        </div>
+        <!-- /.card-body -->
+      </div>
+      <!-- /.card -->
+    </div>
+  </div>
+
 @endsection
